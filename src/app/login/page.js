2687041383af33bbca970b53171426be25b5b706
lordyemight <lordyemight@gmail.com>
2023-12-07 09:23:08 +0100

    hopeful2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -14,7 +14,7 @@ import useProfileStore from "@/store/profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import api from "@/utils/api";
-import { signIn } from 'next-auth/react';
+// import { signIn } from 'next-auth/react';
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -22,10 +22,10 @@ const Login = () => {
   const [visible, setVisible] = useState(false);
   const [loginError, setLoginError] = useState("");
   const router = useRouter();
-  
-  const handleGoogleSignIn = () => {
-    signIn('google');
-  };
+
+  // const handleGoogleSignIn = () => {
+  //   signIn('google');
+  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -225,7 +225,7 @@ const Login = () => {
                   Log In
                 </button>
                 <div className="">
-                  <button onClick={handleGoogleSignIn}  className="border flex justify-center items-center gap-3 font-[700] text-[16px] text-BlueHomz w-full sm:w-[360px] border-BlueHomz hover:border-BlackHomz  rounded-[8px] h-[47px] hover:text-BlackHomz">
+                  <button   className="border flex justify-center items-center gap-3 font-[700] text-[16px] text-BlueHomz w-full sm:w-[360px] border-BlueHomz hover:border-BlackHomz  rounded-[8px] h-[47px] hover:text-BlackHomz">
                     <Image
                       className=""
                       src={"/Social icon.png"}
--- src/pages/api/auth/callback/google.js
+++ src/pages/api/auth/callback/google.js
@@ -1,31 +1,31 @@
-import axios from 'axios';
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+// import axios from 'axios';
+// import NextAuth from 'next-auth';
+// import Providers from 'next-auth/providers';
 
-export default NextAuth({
-  providers: [
-    Providers.Google({
-      clientId: "179490795092-auflgma1odpjdhheetadk6smcgcqvn1e.apps.googleusercontent.com",
-      clientSecret: "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",
-    }),
-  ],
-  callbacks: {
-    async signIn(user, account, profile) {
-        console.log(user)
-      const response = await axios.post('http://your-backend-server/api/auth/login', {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: user.email }),
-      });
+// export default NextAuth({
+//   providers: [
+//     Providers.Google({
+//       clientId: "179490795092-auflgma1odpjdhheetadk6smcgcqvn1e.apps.googleusercontent.com",
+//       clientSecret: "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",
+//     }),
+//   ],
+//   callbacks: {
+//     async signIn(user, account, profile) {
+//         console.log(user)
+//       const response = await axios.post('http://your-backend-server/api/auth/login', {
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ email: user.email }),
+//       });
 
-      const data = await response.json();
+//       const data = await response.json();
 
-      if (data.statuscode === 201) {
-        return true; // Allow the sign-in
-      } else {
-        return false; // Block the sign-in
-      }
-    },
-  },
-});
+//       if (data.statuscode === 201) {
+//         return true; // Allow the sign-in
+//       } else {
+//         return false; // Block the sign-in
+//       }
+//     },
+//   },
+// });
