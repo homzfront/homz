59b40ef68f87dc3acdcc757092d1b4e63b28486c
lordyemight <lordyemight@gmail.com>
2023-12-22 14:13:58 +0100

    let us hope all is done four links4planII

--- src/pages/dashboard/enterprise/notificationSidebar/sidebar/sidebar.js
+++ src/pages/dashboard/enterprise/notificationSidebar/sidebar/sidebar.js
@@ -1,6 +1,7 @@
 "use client";
 import useConversationStore from "@/store/useConversationStore";
 import Image from "next/image";
+import Link from "next/link";
 
 const Sidebar = () => {
   const { Data, selectedTenantId } = useConversationStore();
