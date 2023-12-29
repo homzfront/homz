2d50f6b8eb613c207533b85ff64383aebc92abae
lordyemight <lordyemight@gmail.com>
2023-12-29 02:53:54 +0100

    tenant

--- src/pages/landingPageTenant/ContactCard.js
+++ src/pages/landingPageTenant/ContactCard.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-const image = require('../images/image.png');
+const image = require('../images/image.png') || [];
 
 const ContactCard = () => {
   const [rating, setRating] = useState(0);
