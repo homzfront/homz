10d07ab93264cb7a226867560a0f0fe4d5b52174
lordyemight <lordyemight@gmail.com>
2024-01-19 11:30:44 +0100

    api for public

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
+import "dotenv/config"
 // import Cookies from 'js-cookie';
 
 // Create an instance of Axios with custom configuration
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',  // Set your base URL here
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,  // Set your base URL here
   headers: {
     'Content-Type': 'application/json',
     // You can set other headers as needed
   },
   withCredentials: true,
 });
-
 
 
 export default api;
