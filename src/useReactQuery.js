fab142498ff538f2c8d6335ae6b264ad26518921
lordyemight <lordyemight@gmail.com>
2024-01-21 05:39:48 +0100

    react-query-doneVI

--- src/useReactQuery.js
+++ src/useReactQuery.js
@@ -1,18 +1,24 @@
-"use client"
+"use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
+import EstateInfo from "./pages/dashboard/enterprise/estates/estateInfo/estateInfo";
+import EstateInfoII from "./pages/dashboard/enterprise/estates/estateInfo/components/estateInfo";
+import PropertyDetails from "./pages/dashboard/owner/propertylisting/property/components/propertyDetails";
+import Property from "./pages/dashboard/owner/propertylisting/property/property";
+import Estate from "./pages/dashboard/enterprise/estates/estates";
 
-
-
-
-export const ReactQueryProvider = ({ children }) => {
+export const ReactQueryProvider = () => {
   const queryClient = new QueryClient();
   return (
     <div>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false}/>
-        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+        <EstateInfo />
+        <EstateInfoII />
+        <PropertyDetails/>
+        <Property/>
+      <Estate/>
       </QueryClientProvider>
     </div>
   );
