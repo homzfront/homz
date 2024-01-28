54302cb934808f9d22803193f438ebbdecd353c6
lordyemight <lordyemight@gmail.com>
2024-01-28 08:23:55 +0100

    let us fix query

--- src/utils/useReactQuery.js
+++ src/utils/useReactQuery.js
@@ -2,9 +2,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
 
 export const ReactQueryProvider = ({children}) => {
+   const queryClient = new QueryClient();
    return (
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
