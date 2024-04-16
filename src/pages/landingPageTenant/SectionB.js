1e740172ac9faf5a5338fc015de840285ad8f4c1
lordyemight <lordyemight@gmail.com>
2024-04-16 14:50:05 +0100

    footer & icons

--- src/pages/landingPageTenant/SectionB.js
+++ src/pages/landingPageTenant/SectionB.js
@@ -84,12 +84,7 @@ const SectionB = () => {
                   <div
                     className={`m-auto rounded-full h-[52px] w-[52px] flex justify-center items-center bg-blue-100`}
                   >
-                    <Image
-                      src={card.icon}
-                      height={32}
-                      width={32}
-                      alt={`${card.title}-img`}
-                    />
+                    <>{card.icon}</>
                   </div>
                   <div className="flex flex-col justify-between items-center h-max">
                     <p className="font-[700] text-[20px] text-center text-BlackHomz">
