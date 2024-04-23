b96d10d61748b8b842003eab054a6e83b34b6fc1
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2024-04-23 13:06:06 +0100

    Update page.js

--- src/app/user_homepage/PreviewProperty/page.js
+++ src/app/user_homepage/PreviewProperty/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {Suspense} from "react";
 import ViewProperty from "./ViewProperty";
 import { useSearchParams } from "next/navigation";
 import Loading from "../components/loading";
