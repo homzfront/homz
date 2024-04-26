638017c3854c1e50e560b300412ae1e3b33d2347
lordyemight <lordyemight@gmail.com>
2024-04-26 12:58:45 +0100

    protected completed

--- src/components/auth/protectedRoutes.js
+++ src/components/auth/protectedRoutes.js
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
-import withAuth from '@/utils/withAuth'
 import { Plus_Jakarta_Sans } from "next/font/google";
+import WithAuth from '@/utils/withAuth';
 
 const plus_Jakarta_Sans = Plus_Jakarta_Sans({
     subsets: ["latin"],
@@ -15,5 +15,5 @@ const protectedRoutes =  ({children}) => {
     )
 }
 
-export default withAuth(protectedRoutes, ["/", "/about"]);
+export default WithAuth(protectedRoutes, ["/", "/about"]);
 
--- src/utils/withAuth.js
+++ src/utils/withAuth.js
@@ -1,35 +1,34 @@
-"use client"
 import useProfileStore from "@/store/profile";
 import { useRouter, usePathname } from "next/navigation";
 import { useLayoutEffect, useState } from "react";
 
 export default function WithAuth(Component) {
-    return function WithAuth(props) {
-        const { isLoggedIn } = useProfileStore();
-        const [loading, setLoading] = useState(true);
-        const route = useRouter();
-        const path = usePathname();
-        console.log(isLoggedIn)
-        console.log(path);
-        useLayoutEffect(() => {
-            const checkAuth = async () => {
-                if (!isLoggedIn) {
-                    route.push("/")
-                } else {
-                    if (path === "/login" || path === "/register") {
-                        route.push("/") // Redirect to homepage if trying to access /login or /register while logged in
-                    }
-                }
-                setLoading(false);
-            };
-            checkAuth();
-        }, []);
+  return function WithAuth(props) {
+    const { isLoggedIn } = useProfileStore();
+    const [loading, setLoading] = useState(true);
+    const route = useRouter();
+    const path = usePathname();
 
-        if (loading) {
-            return <div />; // Return an empty div until the authentication check is complete
+    useLayoutEffect(() => {
+      const checkAuth = async () => {
+        if (!isLoggedIn) {
+          if (path !== "/login" && path !== "/register") {
+            route.push("/login"); // Redirect to login page if not logged in and trying to access a protected page
+          }
         } else {
-            return <Component {...props} />;
+          if (path === "/login" || path === "/register") {
+            route.push("/"); // Redirect to homepage if logged in and trying to access /login or /register
+          }
         }
+        setLoading(false);
+      };
+      checkAuth();
+    }, []);
 
+    if (loading) {
+      return <div />; // Return an empty div until the authentication check is complete
+    } else {
+      return <Component {...props} />;
     }
+  };
 }
