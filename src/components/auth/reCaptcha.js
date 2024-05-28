fb55c477cd6c96e4ce8af0a740224c4f24e34876
lordyemight <lordyemight@gmail.com>
2024-05-28 17:40:17 +0100

    marketed by reCaptcha

--- src/components/auth/reCaptcha.js
+++ src/components/auth/reCaptcha.js
@@ -6,7 +6,7 @@ import "dotenv/config";
 const ReCaptcha = ({ onChange }) => {
   return (
     <ReCAPTCHA
-      sitekey={process.env.NEXT_PUBLIC_CAPTCHA}
+      sitekey={process.env.NEXT_PUBLIC_CAPTCHA ? process.env.NEXT_PUBLIC_CAPTCHA : "6Lc9s-opAAAAAEZpd9dnyHr2PwqnMarBED6YDyeN"}
       onChange={onChange}
     />
   );
