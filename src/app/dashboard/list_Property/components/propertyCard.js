81dfc05cb4e1aff8fe5bd1627632ae5753207e3e
lordyemight <lordyemight@gmail.com>
2024-07-10 17:16:19 +0100

    fixed listing

--- src/app/dashboard/list_Property/components/propertyCard.js
+++ src/app/dashboard/list_Property/components/propertyCard.js
@@ -1,21 +1,26 @@
+// components/PropertyCard.js
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import Button from "./button";
-import { Properties } from "./Properties";
+import Pagination from "@/components/general/pagination";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import capitalizeFirstLetter from "@/utils/capitalizeFirstLetter";
+import SkeletonLoader from "./skeletonLoader"
 
-const PropertyCard = ({ Property, state, setDataProperties }) => {
-  const ITEMS_PER_PAGE = 8;
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(Property?.length / ITEMS_PER_PAGE);
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentProperties = Property?.slice(startIndex, endIndex);
+const PropertyCard = ({
+  firstThreePages,
+  currentPage,
+  totalPages,
+  handleNext,
+  handlePageClick,
+  handlePrev,
+  lastThreePages,
+  currentData,
+  loading,
+}) => {
   const [isMenuOpen, setIsMenuOpen] = useState(
-    Array(currentProperties.length).fill(false)
+    Array(currentData?.length).fill(false)
   );
   const router = useRouter();
   const handleMenuToggle = (index) => {
@@ -23,126 +28,110 @@ const PropertyCard = ({ Property, state, setDataProperties }) => {
     newMenuState[index] = !newMenuState[index];
     setIsMenuOpen(newMenuState);
   };
-  const handleNext = () => {
-    setCurrentPage((prev) => Math.min(prev + 1, totalPages));
-  };
-
-  const handlePrev = () => {
-    setCurrentPage((prev) => Math.max(prev - 1, 1));
-  };
-
-  const handlePageClick = (page) => {
-    setCurrentPage(page);
-  };
-  const firstThreePages = Array.from(
-    { length: Math.min(totalPages, 3) },
-    (_, index) => index + 1
-  );
-  const lastThreePagesStart = Math.max(totalPages - 2, 1);
-  const lastThreePages = Array.from(
-    { length: Math.min(totalPages, 3) },
-    (_, index) => lastThreePagesStart + index
-  );
-
 
   return (
     <div className="w-full flex flex-col gap-[64px] pt-6 justify-center items-center">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
-        {currentProperties.map((property, index) => (
-          <div
-            className="flex flex-col w-[100%] md:w-[224px] md:h-[281px] rounded-[12px] shadow-md"
-            key={index}
-          >
-            <div
-              className="cursor-pointer w-[100%] md:w-[224px] md:h-[168px] rounded-[10px] "
-            >
-              <div className="w-[100%] relative h-[100.55px] md:h-full md:w-full">
-                <Link
-                  className="cursor-pointer text-[14px]"
-                  href={`/dashboard/list_Property/PreviewProperty/${property?._id}`}
-                >
-                  <Image
-                    src={property?.coverPhoto?.url}
-                    alt=""
-                    width={264}
-                    height={168}
-                    className="w-[100%] h-[100.55px] md:h-full md:w-full object-cover relative z-0 rounded-t-[12px]"
-                  />
-                </Link>
-              </div>
-            </div>
-            <div className="flex flex-col px-2 pb-2  pt-2 md:pt-5 gap-[5px] md:gap-[2px]">
-              <div className="flex justify-between items-center mb-2 text-[11px] md:text-[16px]">
-                <p className="text-[#006AFF] truncate font-[700] leading-[13.86px] md:leading-[24px] text-center">
-                  {property?.name ? property?.name : property?.title}
-                </p>
-                <div className="relative">
-                  <Image
-                    src="/static/images/verticatDotsIcon.svg"
-                    alt=""
-                    width={20}
-                    height={20}
-                    className="h-[15px] w-[15px] md:w-[20px] md:h-[20px] cursor-pointer"
-                    onClick={() => handleMenuToggle(index)}
-                  />
-                  {isMenuOpen[index] && (
-                    <button
-                      className="absolute md:top-[1.55rem] top-[1rem] right-[5px] w-[80px] md:right-[6px] mt-2 h-[37px] py-2  md:w-[212px] bg-white shadow-md rounded-md"
-                      onClick={() =>
-                        router.push(
-                          `/dashboard/list_Property/edit_property/${property?._id}`
-                        )
-                      }
+      {loading ? (
+        <SkeletonLoader count={currentData?.length || 8} />
+      ) : (
+        <>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
+            {currentData?.map((property, index) => (
+              <div
+                className="flex flex-col w-[100%] md:w-[224px] md:h-[281px] rounded-[12px] shadow-md"
+                key={index}
+              >
+                <div className="cursor-pointer w-[100%] md:w-[224px] md:h-[168px] rounded-[10px]">
+                  <div className="w-[100%] relative h-[100.55px] md:h-full md:w-full">
+                    <Link
+                      className="cursor-pointer text-[14px]"
+                      href={`/dashboard/list_Property/PreviewProperty/${property?._id}`}
                     >
-                      Edit details
-                    </button>
-                  )}
+                      <Image
+                        src={property?.coverPhoto?.url}
+                        alt=""
+                        width={264}
+                        height={168}
+                        className="w-[100%] h-[100.55px] md:h-full md:w-full object-cover relative z-0 rounded-t-[12px]"
+                      />
+                    </Link>
+                  </div>
+                </div>
+                <div className="flex flex-col px-2 pb-2 pt-2 md:pt-5 gap-[5px] md:gap-[2px]">
+                  <div className="flex justify-between items-center mb-2 text-[11px] md:text-[16px]">
+                    <p className="text-[#006AFF] truncate font-[700] leading-[13.86px] md:leading-[24px] text-center">
+                      {property?.name ? property?.name : property?.title}
+                    </p>
+                    <div className="relative">
+                      <Image
+                        src="/static/images/verticatDotsIcon.svg"
+                        alt=""
+                        width={20}
+                        height={20}
+                        className="h-[15px] w-[15px] md:w-[20px] md:h-[20px] cursor-pointer"
+                        onClick={() => handleMenuToggle(index)}
+                      />
+                      {isMenuOpen[index] && (
+                        <button
+                          className="absolute md:top-[1.55rem] top-[1rem] right-[5px] w-[80px] md:right-[6px] mt-2 h-[37px] py-2  md:w-[212px] bg-white shadow-md rounded-md"
+                          onClick={() =>
+                            router.push(
+                              `/dashboard/list_Property/edit_property/${property?._id}`
+                            )
+                          }
+                        >
+                          Edit details
+                        </button>
+                      )}
+                    </div>
+                  </div>
+
+                  <p className="flex gap-1 items-center">
+                    <Image
+                      src="/static/images/Location_Vector.svg"
+                      alt=""
+                      width={12}
+                      height={15.85}
+                      className="h-[12px] w-[12px] md:w-[12px] md:h-[15.85px]"
+                    />
+                    <span className="text-[9px] md:text-[11px] font-[500]">
+                      {`${property?.area}, ${property?.state}`}
+                    </span>
+                  </p>
+                  <div className="flex justify-between items-center">
+                    <p className="text-[9px] md:font-[700] leading-[11.34px] font-[500] md:leading-[13.86px]  font-['Plus Jakarta Sans'] text-[9px] md:text-[11px] flex items-center">
+                      <Image
+                        src="/static/images/nairaIcon.svg"
+                        alt=""
+                        width={17}
+                        height={25}
+                        className="h-[12px] w-[12px] md:w-[15px] md:h-[25px]"
+                      />
+                      <span className="pl-1">
+                        {Number(property?.price).toLocaleString()}{" "}
+                      </span>
+                      <span className="text-[16px] font-[400] md:text-[18px] md:font-[500] ml-1 pt-1 text-[#4E4E4E]">
+                        {capitalizeFirstLetter(property?.paymentType)}
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
-
-              <p className="flex gap-1 items-center">
-                <Image
-                  src="/static/images/Location_Vector.svg"
-                  alt=""
-                  width={12}
-                  height={15.85}
-                  className="h-[12px] w-[12px] md:w-[12px] md:h-[15.85px] "
-                />
-                <span className="text-[9px] md:text-[11px] font-[500]">
-                  {`${property?.area}, ${property?.state}`}
-                </span>
-              </p>
-              <div className="flex justify-between items-center">
-                <p className=" text-[9px] md:font-[700] leading-[11.34px] font-[500] md:leading-[13.86px]  font-['Plus Jakarta Sans'] text-[9px] md:text-[11px] flex items-center ">
-                  <Image
-                    src="/static/images/nairaIcon.svg"
-                    alt=""
-                    width={17}
-                    height={25}
-                    className="h-[12px] w-[12px] md:w-[15px] md:h-[25px]"
-                  />
-                  <span className="pl-1">
-                    {Number(property?.price).toLocaleString()}{" "}
-                  </span>
-                  <span className="text-[16px] font-[400] md:text-[18px] md:font-[500] ml-1 pt-1 text-[#4E4E4E]">
-                    {capitalizeFirstLetter(property?.paymentType)}
-                  </span>
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-
-      <Button
-        firstThreePages={firstThreePages}
-        currentPage={currentPage}
-        lastThreePages={lastThreePages}
-        totalPages={totalPages}
-        handleNext={handleNext}
-        handlePageClick={handlePageClick}
-        handlePrev={handlePrev}
-      />
+          {currentData &&
+            <Pagination
+              firstThreePages={firstThreePages}
+              currentPage={currentPage}
+              lastThreePages={lastThreePages}
+              totalPages={totalPages}
+              handleNext={handleNext}
+              handlePageClick={handlePageClick}
+              handlePrev={handlePrev}
+            />
+          }
+        </>
+      )}
     </div>
   );
 };
--- src/app/dashboard/list_Property/page.js
+++ src/app/dashboard/list_Property/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,129 +8,176 @@ import LoadingII from "./components/loading";
 import useClickOutside from "@/utils/clickOutside";
 import useProfileListingMe from "@/store/listingStore/useProfileListingMe";
 import BusinessAlert from "@/components/icons/businessAlert";
+import api from "@/utils/api";
 
 const List_Property = () => {
+  const [currentData, setData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [propertyLoader, setLoading] = useState(false);
   const [openModalForBusi, setOpenModalForBusi] = useState(false);
-  const dropdownRef = useClickOutside(() => setOpenModalForBusi(false)); // Use the custom hook
-  const { propertyListedAll, loading, fetchData } = usePropertyStore();
+  const dropdownRef = useClickOutside(() => setOpenModalForBusi(false)); 
+  const [dataLength, setLength] = useState(0);
+  const { propertyListedAll, loading, fetchData: fetchProperties } = usePropertyStore();
   const { data: profile, fetchData: fetchProfile } = useProfileListingMe();
+  const [filters, setFilters] = useState({
+    propertyType: null,
+    search: null,
+    numberOfBathrooms: null,
+  });
+
+  const handleFilterChange = (key, value) => {
+    setFilters((prevFilters) => ({ ...prevFilters, [key]: value }));
+  };
+
+  const handleSearch = (query, label) => {
+    handleFilterChange(label, query);
+  };
+
+  const handleSearchChange = (e) => {
+    const { value } = e.target;
+    handleFilterChange("search", value);
+  };
+
+  const reset = () => {
+    setFilters({
+      propertyType: null,
+      numberOfBathrooms: null,
+      search: null,
+    });
+    setCurrentPage(1);
+  };
 
   useEffect(() => {
-    fetchData();
+    fetchProperties();
     fetchProfile();
   }, []);
-  const data = propertyListedAll
+
+  useEffect(() => {
+    const fetchFilteredData = async (page) => {
+      const query = {};
+      Object.keys(filters).forEach((key) => {
+        if (filters[key]) {
+          query[key] = filters[key];
+        }
+      });
+      setLoading(true);
+      try {
+        const response = await api.get(`/properties/listing-property?page=${page}&${new URLSearchParams(query).toString()}`);
+        const result = response?.data;
+        setData(result?.data?.results[0]?.data);
+        setLength(result?.data?.results[0]?.metadata[0]?.total)
+        setTotalPages(result?.data?.results[0]?.metadata[0]?.total > 8 ? Math.ceil(result?.data?.results[0]?.metadata[0]?.total / 8) : 1);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setData(null);
+      }
+    };
+
+    fetchFilteredData(currentPage);
+  }, [currentPage, filters]);
+
+  const handlePageClick = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const firstThreePages = [1, 2, 3];
+  const lastThreePages = [totalPages - 2, totalPages - 1, totalPages];
 
   const handleOpenModal = () => {
     setOpenModalForBusi(true);
   };
 
+  const data = propertyListedAll;
+
   return (
     <div className="dashboard w-full">
-      {
-        openModalForBusi &&
-        <div
-          className="fixed inset-0 flex items-center justify-center z-20 bg-black bg-opacity-30">
+      {openModalForBusi && (
+        <div className="fixed inset-0 flex items-center justify-center z-20 bg-black bg-opacity-30">
           <div ref={dropdownRef} className="bg-white w-[320px] md:w-[464px] h-[290px] rounded-[12px] flex flex-col p-8 items-center justify-around">
             <BusinessAlert />
-            <p className="text-[16px] md:text-[20px] font-[700] text-BlackHomz">
-              Update Business Information
-            </p>
+            <p className="text-[16px] md:text-[20px] font-[700] text-BlackHomz">Update Business Information</p>
             <p className="text-[14px] md:text-[16px] font-[400] text-GrayHomz text-center">
               Kindly upload your business certification in order to list more properties
             </p>
-            <Link
-              href={"/dashboard/list_Property/Profile?tab=business"}
-              className="w-full h-[48px] bg-BlueHomz rounded-[4px] flex items-center justify-center"
-            >
-              <span className="text-white text-[14px] md:text-[16px] font-[700]">
-                Upload Certificate
-              </span>
+            <Link href={"/dashboard/list_Property/Profile?tab=business"} className="w-full h-[48px] bg-BlueHomz rounded-[4px] flex items-center justify-center">
+              <span className="text-white text-[14px] md:text-[16px] font-[700]">Upload Certificate</span>
             </Link>
           </div>
         </div>
-      }
-      {data && <>
-        <div className={` ${data?.length === 0 ? "hidden" : ""} flex w-[80%] items-center gap-6 mt-[-15px] md:mt-0 mb-6 pt-2 md:mb-0`}>
-          <div className="flex gap-2 items-center md:mb-4">
-            <p className="font-[500] leading-[30px] md:text-[20px]">
-              Listed Properties
-            </p>
-
-            <p className="text-[#006AFF] md:text-[18px] bg-[#EEF5FF] px-[8px] h-[28px] md:h-[35px] py-[4px] rounded-[8px]">
-              {data?.length > 0 ? data?.length : 0}
-            </p>
-          </div>
-          { data?.length > 0 &&
-            (profile?.businessInfo?.isVerified === 'unverified' || profile?.businessInfo?.isVerified === 'pending' || profile?.businessInfo?.isVerified === 'rejected') ?
+      )}
+      {data && (
+        <>
+          <div className={`${data?.length === 0 ? "hidden" : ""} flex w-[80%] items-center gap-6 mt-[-15px] md:mt-0 mb-6 pt-2 md:mb-0`}>
+            <div className="flex gap-2 items-center md:mb-4">
+              <p className="font-[500] leading-[30px] md:text-[20px]">Listed Properties</p>
+              <p className="text-[#006AFF] md:text-[18px] bg-[#EEF5FF] px-[8px] h-[28px] md:h-[35px] py-[4px] rounded-[8px]">{dataLength}</p>
+            </div>
+            {data?.length > 0 &&
+              (profile?.businessInfo?.isVerified === 'unverified' ||
+                profile?.businessInfo?.isVerified === 'pending' ||
+                profile?.businessInfo?.isVerified === 'rejected') ? (
               <div>
-                <Image
-                  src="/static/images/addButton.svg"
-                  alt=""
-                  height={28}
-                  width={28}
-                  className="md:hidden rounded-[8px]"
-                  onClick={handleOpenModal}
-                />
+                <Image src="/static/images/addButton.svg" alt="" height={28} width={28} className="md:hidden rounded-[8px]" onClick={handleOpenModal} />
               </div>
-              :
+            ) : (
               <Link href="/dashboard/list_Property/addProperty">
-                <Image
-                  src="/static/images/addButton.svg"
-                  alt=""
-                  height={28}
-                  width={28}
-                  className="md:hidden rounded-[8px]"
-                />
+                <Image src="/static/images/addButton.svg" alt="" height={28} width={28} className="md:hidden rounded-[8px]" />
               </Link>
-          }
-        </div>
-        {loading ?
-          <div className="h-screen flex justify-center items-center">
-            <LoadingII />
+            )}
           </div>
-          : data && data.length < 1 ? (
+          {loading ? (
+            <div className="h-screen flex justify-center items-center">
+              <LoadingII />
+            </div>
+          ) : data && data.length < 1 ? (
             <div>
-              <p className="md:hidden font-[400] leading-[17.64px] text-[#A9A9A9] text-[14px] mt-0 md:mt-2">
-                List your properties so Tenants can see them.
-              </p>
+              <p className="md:hidden font-[400] leading-[17.64px] text-[#A9A9A9] text-[14px] mt-0 md:mt-2">List your properties so Tenants can see them.</p>
               <div className="flex flex-col items-center justify-center pt-[10rem] md:pt-0">
-                <div className="flex flex-col items-center justify-center md:h-[412px] gap-[20px] ">
-                  <Image
-                    src="/static/images/PropertyLister.svg"
-                    alt=""
-                    height={121}
-                    width={121}
-                    className="rounded-[8px] mx-auto"
-                  />
-                  <p className="text-[23px] md:text-[36px] text-[700] leading-[28.98px] md:leading-[45px] text-[#006AFF] ">
-                    Get Started
-                  </p>
-                  { }
-                  <p className="hidden md:block text-[#4E4E4E] leading-[27px]  w-[338px] md:w-full text-center">
-                    List your properties so Tenants can see them.
-                  </p>
-                  <Link
-                    href="/dashboard/list_Property/addProperty"
-                    className="w-[338px] flex gap-1 md:w-[165px] h-[48px] md:p-[12px] items-center justify-center rounded-[4px] text-white bg-[#006AFF]"
-                  >
-                    <Image
-                      src="/static/images/white-add.svg"
-                      alt=""
-                      height={16}
-                      width={16}
-                      className=""
-                    />
+                <div className="flex flex-col items-center justify-center md:h-[412px] gap-[20px]">
+                  <Image src="/static/images/PropertyLister.svg" alt="" height={121} width={121} className="rounded-[8px] mx-auto" />
+                  <p className="text-[23px] md:text-[36px] text-[700] leading-[28.98px] md:leading-[45px] text-[#006AFF]">Get Started</p>
+                  <p className="hidden md:block text-[#4E4E4E] leading-[27px] w-[338px] md:w-full text-center">List your properties so Tenants can see them.</p>
+                  <Link href="/dashboard/list_Property/addProperty" className="w-[338px] flex gap-1 md:w-[165px] h-[48px] md:p-[12px] items-center justify-center rounded-[4px] text-white bg-[#006AFF]">
+                    <Image src="/static/images/white-add.svg" alt="" height={16} width={16} className="" />
                     <span>List Properties</span>
                   </Link>
                 </div>
               </div>
             </div>
           ) : (
-            <Property property={data} />
+            <Property
+              property={data}
+              firstThreePages={firstThreePages}
+              currentPage={currentPage}
+              totalPages={totalPages}
+              handleNext={handleNext}
+              handlePageClick={handlePageClick}
+              handlePrev={handlePrev}
+              lastThreePages={lastThreePages}
+              currentData={currentData}
+              loading={propertyLoader}
+              filters={filters}
+              handleSearch={handleSearch}
+              handleSearchChange={handleSearchChange}
+              reset={reset}
+            />
           )}
-      </>
-      }
+        </>
+      )}
     </div>
   );
 };
