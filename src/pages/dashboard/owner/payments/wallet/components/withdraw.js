3f63e9c3ba0ffe4d6d408705310f2e1dca832470
lordyemight <lordyemight@gmail.com>
2024-07-11 14:11:02 +0100

    withdrawal

--- src/pages/dashboard/owner/payments/wallet/components/withdraw.js
+++ src/pages/dashboard/owner/payments/wallet/components/withdraw.js
@@ -15,6 +15,7 @@ import UseBankDataStore from "@/store/propertyOwnerStore/useBankDataStore";
 import Eye from "@/components/icons/Eye";
 import BashedEye from "@/components/icons/BashedEye";
 import CustomizeModal from "@/components/mainmenu/CustomizedModal";
+import addCommasToNumber from "@/utils/addCommasToNumber";
 
 const Withdraw = ({
   illuminateWallet,
@@ -137,7 +138,8 @@ const Withdraw = ({
               height={48}
               width={48}
             />
-            <h1 className="text-BlackHomz text-center font-[700] text-[20px]">withdrawal successful</h1>
+            <h1 className="text-BlackHomz text-center font-[700] text-[20px]">Withdrawal Processing</h1>
+            <p className="text-GrayHomz text-center font-[400] text-[16px]">Your withdrawal of {addCommasToNumber(amount)} will be credited into your account in 24 hours</p>
             <button
               onClick={() => {
                 setSuccessModal(false)
