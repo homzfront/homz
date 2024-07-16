f405db437eb6ee5dcfa23085db8fb09d6e5919e1
lordyemight <lordyemight@gmail.com>
2024-07-16 12:20:48 +0100

    reminder single profile

---
+++ src/app/dashboard/enterprise-property/tenants/reminder/components/customizeSettings.js
@@ -0,0 +1,266 @@
+import React, { useState } from 'react';
+import DropDownReminder from './dropDownReminder';
+import DropDownChannel from './dropDownChannel';
+import CustomizeModal from '@/components/mainmenu/CustomizedModal';
+import Image from 'next/image';
+import RichTextEditorEmail from './richTextEditorEmail';
+import RichTextEditorSMS from './richTextEditorSMS';
+import RichTextEditorInApp from './richTextEditorInApp';
+
+const CustomizeSettings = ({ setCustomizeSettings, data }) => {
+    const [time, setTime] = useState(null);
+    const [dueDate, setDueDate] = useState(null);
+    const [channels, setChannels] = useState([]);
+    const [modalConfirmChanges, setModalConfirmChanges] = useState(false);
+    const [modalSave, setModalSave] = useState(false);
+    const [openCompleted, setOpenCompleted] = useState(false);
+
+    const options = [
+        { id: 1, label: "6 months before due date" },
+        { id: 2, label: "3 months before due date" },
+        { id: 3, label: "1 month before due date" },
+        { id: 4, label: "7 days before due date" },
+        { id: 5, label: "3 days before due date" },
+        { id: 6, label: "1 day before due date" },
+        { id: 7, label: "Due date" },
+        { id: 8, label: "Post due date" }
+    ];
+
+    const optionII = [
+        { id: 1, label: "All" },
+        { id: 2, label: "In-App" },
+        { id: 3, label: "Email" },
+        { id: 4, label: "SMS" },
+    ];
+
+    const isSMSPresent = channels.includes('SMS');
+    const isEmailPresent = channels.includes('Email');
+    const isInAppPresent = channels.includes('In-App');
+
+    return (
+        <div>
+            <CustomizeModal isOpen={modalConfirmChanges}>
+                {openCompleted ? (
+                    <div className="p-2 m-auto bg-white h-auto rounded-md">
+                        <div className="mt-[-10px] md:w-[464px] flex flex-col justify-around p-8 items-center gap-3">
+                            <Image
+                                src="/static/dashboard/enterprisemanager/dashboard/Featured-icon.png"
+                                alt=""
+                                height={48}
+                                width={48}
+                            />
+                            <h1 className="text-BlackHomz text-center font-[700] text-[20px]">Reminder Settings Saved</h1>
+                            <p className="text-[16px] font-[400] text-GrayHomz text-center">Tenants will now receive reminders to prompt swift rent payment</p>
+                            <button
+                                onClick={() => {
+                                    setOpenCompleted(false);
+                                    setModalConfirmChanges(false);
+                                }}
+                                className="h-[48px] rounded-md w-full bg-BlueHomz text-white text-[16px] font-[700]"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="max-w-[464px] p-2 m-auto bg-white h-auto rounded-md">
+                        <div className="flex flex-col justify-around items-center h-full p-6">
+                            <h1 className="text-BlackHomz font-[500] text-[20px] text-center">
+                                Change System Settings
+                            </h1>
+                            <p className="text-[16px] font-[500] text-GrayHomz text-center">
+                                Are you sure you want to change the system’s default settings?
+                            </p>
+                            <button
+                                onClick={() => setOpenCompleted(true)}
+                                className="mt-2 h-[48px] rounded-md w-full bg-BlueHomz text-white text-[16px] font-[500]"
+                            >
+                                Proceed
+                            </button>
+                            <button
+                                onClick={() => setModalConfirmChanges(false)}
+                                className="mt-4 h-[48px] rounded-md w-full border border-BlueHomz text-BlueHomz text-[16px] font-[500]"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </CustomizeModal>
+            <CustomizeModal isOpen={modalSave}>
+                <div className="max-w-[464px] p-2 m-auto bg-white md:h-[245px] rounded-md">
+                    <div className="flex flex-col justify-around items-center h-full p-6">
+                        <h1 className="text-BlackHomz font-[500] text-[20px] text-center">
+                            Restore Default Settings
+                        </h1>
+                        <p className="text-[16px] font-[500] text-GrayHomz text-center">
+                            Are you sure you want to restore the system’s default settings?
+                        </p>
+                        <button
+                            onClick={() => setCustomizeSettings(false)}
+                            className="mt-2 h-[48px] rounded-md w-full bg-BlueHomz text-white text-[16px] font-[500]"
+                        >
+                            Proceed
+                        </button>
+                        <button
+                            onClick={() => setModalSave(false)}
+                            className="mt-4 h-[48px] rounded-md w-full border border-BlueHomz text-BlueHomz text-[16px] font-[500]"
+                        >
+                            No, go back
+                        </button>
+                    </div>
+                </div>
+            </CustomizeModal>
+            <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]">
+                <div className="flex flex-col md:w-[50%] w-full gap-2">
+                    <p className="text-[14px] text-[500] leading-[24px] text-BlueHomz">
+                        Date before/after due date <span className="text-red-600">*</span>
+                    </p>
+                    <p className="text-[13px] leading-[19.5px] text-[400] text-GrayHomz">
+                        Select your preferred date for reminder(s) to be sent out
+                    </p>
+                </div>
+                <div className="md:w-[50%] w-full">
+                    <DropDownReminder
+                        options={options}
+                        onSelect={(option) => setDueDate(option)}
+                        selectOption={
+                            dueDate === null ? "Select reminder date" : dueDate.label
+                        }
+                        className="text-[14px] font-[500] text-GrayHomz2 md:w-[236px] w-full"
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]">
+                <div className="flex flex-col md:w-[50%] w-full gap-2">
+                    <p className="text-[14px] text-[500] leading-[24px] text-BlueHomz">
+                        Time <span className="text-red-600">*</span>
+                    </p>
+                    <p className="text-[13px] leading-[19.5px] text-[400] text-GrayHomz">
+                        Enter your preferred time for reminder(s) to be sent out
+                    </p>
+                </div>
+                <div className="md:w-[50%] w-full">
+                    <input
+                        type="time"
+                        id="remindTime"
+                        name="remindTime"
+                        required
+                        placeholder="00:00 AM"
+                        className="md:w-[236px] w-full h-[55px] rounded-[4px] text-GrayHomz border border-GrayHomz px-2"
+                        onChange={(e) => setTime(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]">
+                <div className="flex flex-col md:w-[50%] w-full  gap-2">
+                    <p className="text-[14px] text-[500] leading-[24px] text-BlueHomz">
+                        Set reminder channel(s) <span className="text-red-600">*</span>
+                    </p>
+                    <p className="text-[13px] leading-[19.5px] text-[400] text-GrayHomz">
+                        Select the channels through which your tenants would receive rent due reminders.
+                    </p>
+                </div>
+                <div className="md:w-[50%] w-full">
+                    <DropDownChannel
+                        options={optionII}
+                        onSelect={(options) => setChannels(options)}
+                        selectOption={
+                            channels.length === 0 ? "Select Channel(s)" : channels.map((channel) => channel.label).join(", ")
+                        }
+                        className="text-[14px] font-[500] text-GrayHomz2 md:w-[236px] w-full"
+                    />
+                </div>
+            </div>
+            {isEmailPresent && (
+                <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]">
+                    <div className="flex flex-col md:w-[50%] w-full gap-2">
+                        <p className="text-[14px] text-[500] leading-[24px] text-BlueHomz">
+                            Email Reminder Message<span className="text-red-600">*</span>
+                        </p>
+                        <p className="text-[13px] leading-[19.5px] text-[400] text-GrayHomz">
+                            Type in your preferred reminder message for emails
+                        </p>
+                    </div>
+                    <div className='md:w-[50%] w-full flex flex-col gap-2'>
+                        <div className='py-3 px-4 flex items-center border border-GrayHomz text-GrayHomz rounded-[4px]'>
+                            <p className='text-[14px] font-[400]'>{data?.emailReminder}</p>
+                        </div>
+                        <RichTextEditorEmail charLimit={500} text={data?.emailText} />
+                    </div>
+                </div>
+            )}
+            {isSMSPresent && (
+                <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]">
+                    <div className="flex flex-col md:w-[50%] w-full gap-2">
+                        <p className="text-[14px] text-[500] leading-[24px] text-BlueHomz">
+                            SMS Reminder Message<span className="text-red-600">*</span>
+                        </p>
+                        <p className="text-[13px] leading-[19.5px] text-[400] text-GrayHomz">
+                            Type in your preferred reminder message for SMS
+                        </p>
+                    </div>
+                    <div className="md:w-[50%] w-full">
+                        <div className='w-full flex flex-col gap-2'>
+                            <div className='py-3 px-4 flex items-center border border-GrayHomz text-GrayHomz rounded-[4px]'>
+                                <p className='text-[14px] font-[400]'>{data?.sms_label}</p>
+                            </div>
+                            <RichTextEditorSMS charLimit={150} text={data?.sms} />
+                        </div>
+                        <div className="mt-2 flex flex-col md:flex-row md:items-center gap-2 text-[13px] font-[400] text-GrayHomz">
+                            <div>
+                                Send copy to :
+                            </div>
+                            <div className='flex gap-2'>
+                                <div className="flex items-center gap-1">
+                                    <input type="radio" name="sms-copy" />
+                                    <p>Property Manager</p>
+                                </div>
+                                <div className="flex items-center gap-1">
+                                    <input type="radio" name="sms-copy" />
+                                    <p>Property Owner</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {isInAppPresent && (
+                <div className="border-b py-4 w-[100%]">
+                    <div className="flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between">
+                        <div className="flex flex-col md:w-[50%] w-full gap-2">
+                            <p className="text-[14px] text-[500] leading-[24px] text-BlueHomz">
+                                In-App Reminder Notification<span className="text-red-600">*</span>
+                            </p>
+                            <p className="text-[13px] leading-[19.5px] text-[400] text-GrayHomz">
+                                Type in your preferred reminder message for In-App notifications
+                            </p>
+                        </div>
+                        <div className='md:w-[50%] w-full flex flex-col gap-2'>
+                            <div className='py-3 px-4 flex items-center border border-GrayHomz text-GrayHomz rounded-[4px]'>
+                                <p className='text-[14px] font-[400]'>{data?.in_app}</p>
+                            </div>
+                            <RichTextEditorInApp charLimit={150} text={data?.in_app_text} />
+                        </div>
+                    </div>
+                </div>
+            )}
+            <div className="mt-4 pb-4 flex flex-col md:flex-row gap-2 items-center">
+                <button
+                    onClick={() => setModalConfirmChanges(true)}
+                    className="text-[14px] font-[500] w-full md:w-[155px] bg-BlueHomz text-white py-2 rounded-[4px]"
+                >
+                    Save changes
+                </button>
+                <button
+                    onClick={() => setModalSave(true)}
+                    className="text-[14px] font-[500] w-full md:w-[185px] text-BlueHomz hover:border border-BlueHomz py-2 rounded-[4px]"
+                >
+                    Restore default settings
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default CustomizeSettings;
---
+++ src/app/dashboard/enterprise-property/tenants/reminder/components/settings.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react'
+import CustomizeModal from "@/components/mainmenu/CustomizedModal";
+import CustomizeSettings from './customizeSettings';
+
+const Settings = ({ data }) => {
+    const [modalCustom, setModalCustom] = useState(false);
+    const [customizeSettings, setCustomizeSettings] = useState(false);
+
+    return (
+        <div>
+            <CustomizeModal isOpen={modalCustom}>
+                <div className="max-w-[464px] p-2 m-auto bg-white md:h-[245px] rounded-md">
+                    <div className="flex flex-col justify-around items-center h-full p-6">
+                        <h1 className="text-BlackHomz font-[500] text-[20px] text-center">
+                            Change System Settings
+                        </h1>
+                        <p className="text-[16px] font-[500] text-GrayHomz text-center">
+                            Are you sure you want to change the system’s default settings?
+                        </p>
+                        <button
+                            onClick={() => {
+                                setCustomizeSettings(true)
+                                setModalCustom(false)
+                            }}
+                            className="mt-2 h-[48px] rounded-md w-full bg-BlueHomz text-white text-[16px] font-[500]"
+                        >
+                            Proceed
+                        </button>
+                        <button
+                            onClick={() => setModalCustom(false)}
+                            className="mt-4 h-[48px] rounded-md w-full border border-BlueHomz text-BlueHomz text-[16px] font-[500]"
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </CustomizeModal>
+            {
+                customizeSettings ?
+                    <div>
+                        <CustomizeSettings setCustomizeSettings={setCustomizeSettings} data={data}/>
+                    </div>
+                    :
+                    <div>
+                        <div className='flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]'>
+                            <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                <p className='text-[14px] font-[500] text-BlueHomz'>
+                                    Time
+                                </p>
+                                <p className='text-[13px] font-[400] text-BlackHomz'>
+                                    Rent reminder will be sent out to tenants at this time
+                                </p>
+                            </div>
+                            <div className='w-full md:w-[50%]'>
+                                <div className='w-full md:w-[230px] py-3 pl-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                    <p className='text-[14px] font-[500]'>
+                                        12 : 00 PM
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]'>
+                            <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                <p className='text-[14px] font-[500] text-BlueHomz'>
+                                    Reminder channel(s)
+                                </p>
+                                <p className='text-[13px] font-[400] md:w-[70%] text-BlackHomz'>
+                                    Rent reminder(s) will be sent to your tenants through these channels
+                                </p>
+                            </div>
+                            <div className='w-full md:w-[50%]'>
+                                <div className='w-full md:w-[230px] py-3 pl-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                    <p className='text-[14px] font-[500]'>
+                                        In-app, Email, SMS
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]'>
+                            <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                <p className='text-[14px] font-[500] text-BlueHomz'>
+                                    Email Reminder Message
+                                </p>
+                                <p className='text-[13px] font-[400] text-BlackHomz'>
+                                    Tenant(s) will receive this message in their emails
+                                </p>
+                            </div>
+                            <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                <div className='py-3 px-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                    <p className='text-[14px] font-[400]'>{data?.emailReminder}</p>
+                                </div>
+                                <div className='py-3 px-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                    <p className='text-[14px] font-[400] text-justify'>
+                                        {data?.emailText}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between border-b py-4 w-[100%]'>
+                            <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                <p className='text-[14px] font-[500] text-BlueHomz'>
+                                    SMS Reminder Message
+                                </p>
+                                <p className='text-[13px] font-[400] text-BlackHomz'>
+                                    Tenant(s) will receive this message
+                                </p>
+                            </div>
+                            <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                <div className='py-3 px-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                    <p className='text-[14px] font-[400]'>{data?.sms_label}</p>
+                                </div>
+                                <div className='py-3 px-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                    <p className='text-[14px] font-[400] text-justify'>
+                                        {data?.sms}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='py-4 w-[100%]'>
+                            <div className='flex flex-col gap-2 md:gap-0 md:flex-row md:justify-between'>
+                                <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                    <p className='text-[14px] font-[500] text-BlueHomz'>
+                                        In-App Reminder Message
+                                    </p>
+                                    <p className='text-[13px] font-[400] text-BlackHomz'>
+                                        Tenant(s) will receive this message
+                                    </p>
+                                </div>
+                                <div className='w-full md:w-[50%] flex flex-col gap-2'>
+                                    <div className='py-3 px-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                        <p className='text-[14px] font-[400]'>{data?.in_app}</p>
+                                    </div>
+                                    <div className='py-3 px-4 flex items-center bg-GrayHomz6 text-GrayHomz rounded-[4px]'>
+                                        <p className='text-[14px] font-[400] text-justify'>
+                                            {data?.in_app_text}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <button
+                                onClick={() => setModalCustom(true)}
+                                className='mt-4 text-[14px] font-[500] w-[155px] bg-BlueHomz text-white py-2 rounded-[4px]'>
+                                Customize settings
+                            </button>
+                        </div>
+                    </div>
+            }
+        </div>
+    )
+}
+
+export default Settings;
