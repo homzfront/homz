7a96046d974e35f15d21fdadcc5865a9aeb4b5df
lordyemight <lordyemight@gmail.com>
2024-08-20 14:48:14 +0100

    tenant notification

--- src/pages/dashboard/tenant/header/components/reminderNoti.js
+++ src/pages/dashboard/tenant/header/components/reminderNoti.js
@@ -3,7 +3,12 @@ import React from 'react'
 import Link from 'next/link'
 
 const ReminderNoti = ({ noti, closeMenu }) => {
-    const lastElement = noti[noti?.length - 1];
+    const lastElement = noti?.length ? noti[noti?.length - 1] : null;
+
+    if (!lastElement) {
+        return null; 
+    };
+    
     return (
         <div>
             <div className="absolute right-[45px] md:right-[110px] top-[54px] md:top-[70px] w-[320px] md:w-[400px] h-auto bg-whiteblue border border-BlueHomz rounded-[12px] shadow-md px-4 py-2 z-20">
