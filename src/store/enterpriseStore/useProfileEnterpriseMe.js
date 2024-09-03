2eb8900c246a057ae3962c5a83c0690eb361ff95
lordyemight <lordyemight@gmail.com>
2024-09-03 09:01:05 +0100

    api fixes

--- src/store/enterpriseStore/useProfileEnterpriseMe.js
+++ src/store/enterpriseStore/useProfileEnterpriseMe.js
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 import { enterpriseMe } from '@/api/enterpriseManagerService';
-import PromotionHooks from "@/utils/promoteProperty";
 
 
 const useProfileEnterpriseMe = create((set) => ({
@@ -8,12 +7,10 @@ const useProfileEnterpriseMe = create((set) => ({
     loading: true,
     fetchData: async () => {
       try {
-      const res = await PromotionHooks.checkCurrentSubscription();
-        // console.log(res?.data);
         const response = await enterpriseMe();
         const data = response?.data;
         // console.log(data);
-        set({ data: res?.data, loading: false });
+        set({ data: data, loading: false });
       } catch (error) {
         set({ loading: false });
       }
