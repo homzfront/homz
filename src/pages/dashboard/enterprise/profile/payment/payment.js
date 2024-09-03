625c4245aec86e7e6e46cd74ced49522e578e3af
lordyemight <lordyemight@gmail.com>
2024-09-03 07:56:45 +0100

    payments

--- src/pages/dashboard/enterprise/profile/payment/payment.js
+++ src/pages/dashboard/enterprise/profile/payment/payment.js
@@ -99,7 +99,7 @@ useBodyScroll([verified, verifyDelete, removeCard]);
       <p className="font-[700] text-[14px] text-GrayHomz">Enterprise Plan</p>
       <div className=" flex justify-between h-[69px] items-center rounded-lg bg-inputBg px-4 mt-2">
         <p className="font-[500] text-[13px] md:text-[16px] text-GrayHomz2">
-          You’re currently on the {userProfile?.planName} plan, billed {userProfile?.interval === "annually" ? "yearly." : "monthly."}
+          You’re currently on the {userProfile?.planName} plan{userProfile?.interval === "annually" ? ", billed yearly.": userProfile?.interval === "monthly" ? ", billed monthly." : "."}
         </p>
         <Link
           href={"/plans"}
