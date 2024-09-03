53e956a9ebcaea8e61522f74738d2088df2232e8
lordyemight <lordyemight@gmail.com>
2024-09-03 10:36:35 +0100

    profile

--- src/pages/dashboard/enterprise/profile/payment/payment.js
+++ src/pages/dashboard/enterprise/profile/payment/payment.js
@@ -7,7 +7,7 @@ import CollectCardDetails from "./components/collectCardDetails";
 import YesNOModal from "../../tenants/components/yesNOModal";
 import useBodyScroll from "@/utils/useBodyScroll";
 
-const Payment = ({data: userProfile}) => {
+const Payment = ({ data: userProfile }) => {
   const [fillCard, setFillCard] = useState(false);
   const [data, setData] = useState([]);
   const [cvv, setCvv] = useState("");
@@ -20,7 +20,7 @@ const Payment = ({data: userProfile}) => {
   const [verifyII, setVerifyII] = useState(false);
   const [selectedCardId, setSelectedCardId] = useState(null);
 
-useBodyScroll([verified, verifyDelete, removeCard]);
+  useBodyScroll([verified, verifyDelete, removeCard]);
 
   const handleCardNumberChange = (e) => {
     // Remove non-numeric characters from the input
@@ -88,7 +88,7 @@ useBodyScroll([verified, verifyDelete, removeCard]);
     setVerifyDelete(false);
     setRemoveCard(false);
   };
-  
+
   const reversedData = data.slice().reverse();
   const lastData = () => {
     return data[data.length - 1];
@@ -99,7 +99,7 @@ useBodyScroll([verified, verifyDelete, removeCard]);
       <p className="font-[700] text-[14px] text-GrayHomz">Enterprise Plan</p>
       <div className=" flex justify-between h-[69px] items-center rounded-lg bg-inputBg px-4 mt-2">
         <p className="font-[500] text-[13px] md:text-[16px] text-GrayHomz2">
-          You’re currently on the {userProfile?.planName} plan{userProfile?.interval === "annually" ? ", billed yearly.": userProfile?.interval === "monthly" ? ", billed monthly." : "."}
+          You’re currently on the {userProfile?.planName === "" ? "free trial" : userProfile?.planName} plan{userProfile?.interval === "annually" ? ", billed yearly." : userProfile?.interval === "monthly" ? ", billed monthly." : "."}
         </p>
         <Link
           href={"/plans"}
