9559638899ab838c845baf6503f368bcb51de760
lordyemight <lordyemight@gmail.com>
2024-09-04 14:47:35 +0100

    facebook pixels

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import { GoogleTagManager } from '@next/third-parties/google';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Head from 'next/head';
+import useFacebookPixel from "@/utils/useFacebookPixels";
 
 const plus_Jakarta_Sans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -27,10 +28,31 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  useFacebookPixel();
   return (
     <html lang="en">
       <Head>
         <meta name="google-site-verification" content={process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '584379467167441');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=584379467167441&ev=PageView&noscript=1" alt="" />
+        </noscript>
       </Head>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER} />
@@ -50,8 +72,8 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+        <div className={plus_Jakarta_Sans.className}>{children}</div>
       </body>
-      <body className={plus_Jakarta_Sans.className}>{children}</body>
     </html>
   );
 }
