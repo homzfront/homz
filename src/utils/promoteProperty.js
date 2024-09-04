a72c46ab8274f671b2a1fd6282ee1e5b4ca3a9b4
lordyemight <lordyemight@gmail.com>
2024-09-04 16:21:29 +0100

    update facebook pixels

--- src/utils/promoteProperty.js
+++ src/utils/promoteProperty.js
@@ -1,29 +1,20 @@
 import api from "@/utils/api";
-
 async function promoteProperty(date, propertyId, plan, propertyIds) {
-  
   try {
     const endpoint =
       plan === "single"
         ? `/property/promotion/single-property/${propertyId}`
         : `/property/promotion/multiple-property`;
-
     const payload =
       plan === "single" ? { endDate: date } : { endDate: date, propertyIds };
-
     const response = await api.post(endpoint, payload);
     // console.log(response);
     return response.data;
   } catch (error) {
-    // console.log(error)
-    // console.error(
-    //   "Error promoting property:",
-    //   error.response?.data || error.message
-    // );
+    console.log(error);
     return error;
   }
 }
-
 async function stopSinglePromotion(propertyId) {
   try {
     const result = await api.put(
@@ -32,11 +23,11 @@ async function stopSinglePromotion(propertyId) {
     // console.log(result.data)
     return result.data;
   } catch (error) {
-    // console.log(error)
-    // console.error(
-    //   "Error promoting property:",
-    //   error.response?.data || error.message
-    // );
+    console.log(error);
+    console.error(
+      "Error promoting property:",
+      error.response?.data || error.message
+    );
     return error.response?.data || { message: "An unexpected error occurred." };
   }
 }
@@ -48,27 +39,28 @@ async function checkCurrentSubscription() {
     // console.log(results);
     return results;
   } catch (error) {
-    // console.error("Error", error.response?.data || error.message);
+    console.error("Error", error.response?.data || error.message);
     return error;
   }
 }
 async function createSubscription(planName, interval, amount, upgradePlan) {
   // console.log(upgradePlan)
-   try {
-    
-    const results = await api.post(`/subscribe/listingProperty/${upgradePlan? "update":"new"}`, {
-      planName: planName,
-      interval: interval,
-      amount: amount,
-    });
+  try {
+    const results = await api.post(
+      `/subscribe/listingProperty/${upgradePlan ? "update" : "new"}`,
+      {
+        planName: planName,
+        interval: interval,
+        amount: amount,
+      }
+    );
     // console.log(results?.data?.data);
-    return results?.data?.data?.paystackResponse
+    return results?.data?.data?.paystackResponse;
   } catch (error) {
-    // console.error("Error", error.response?.data || error.message);
-    return error.response?.data || { message: "An unexpected error occurred." };
+    console.error("Error", error.response?.data || error.message);
+    return error;
   }
 }
-
 export default {
   stopSinglePromotion,
   promoteProperty,
