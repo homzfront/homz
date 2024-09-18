cce3a98dfac4fe7777d2bd820e68037d9c33cc49
k2lawbaaba <123904253+k2lawbaaba@users.noreply.github.com>
2024-09-18 12:56:28 +0100

    Update ViewProperty.js

--- src/app/dashboard/list_Property/PreviewProperty/ViewProperty.js
+++ src/app/dashboard/list_Property/PreviewProperty/ViewProperty.js
@@ -74,14 +74,7 @@ const ViewProperty = ({ PropertyID }) => {
     }
   }, [propertyData]);
 
-  useEffect(() => {
-    // Update remainder state when combinedData length changes
-    if (combinedData.length === 8) {
-      setRemainder(combinedData.length - 7);
-    }
-  }, [combinedData]);
-
-  const [open, setOpen] = useState(false);
+ 
 
   useEffect(() => {
     const property = Properties.find(({ _id }) => _id === parseInt(PropertyID));
