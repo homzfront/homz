8eb9e0940007a5e9db95fb81fe5642c1d8573d86
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2024-09-20 19:39:16 +0100

    update

--- src/app/dashboard/list_Property/Profile/components/SubscriptionInfo.js
+++ src/app/dashboard/list_Property/Profile/components/SubscriptionInfo.js
@@ -18,7 +18,7 @@ const SubscriptionInfo = () => {
 
   const getCurrentSubscriptionPlan = async () => {
     const response = await PromotionHooks.checkCurrentSubscription();
-    // console.log(response?.data?.data);
+    console.log(response?.data?.data);
     setCurrentPlanData(response?.data?.data);
   };
 
