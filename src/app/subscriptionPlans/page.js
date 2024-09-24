c492d7b21b9924cdb4b92575be9957d5ee34328e
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2024-09-24 10:39:22 +0100

    Update page.js

--- src/app/subscriptionPlans/page.js
+++ src/app/subscriptionPlans/page.js
@@ -1,19 +1,32 @@
 "use client";
-import PricingPlan from './pricingPlan'
-import React from 'react'
+
+import PricingPlan from './pricingPlan';
+import React, { useEffect, useState } from 'react';
 import { redirect } from 'next/navigation';
 
-
 const SubscriptionPage = () => {
-  let jwt = localStorage.getItem("jwt");
-  if(!jwt){
-      redirect("/login");
+  const [token, setToken] = useState(null); // Store token state
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const jwtToken = localStorage.getItem("jwt");
+      if (!jwtToken) {
+        redirect("/login"); // Redirect to login if no token
+      } else {
+        setToken(jwtToken); // Set token if found
+      }
+    }
+  }, []);
+
+  if (!token) {
+    return null; 
   }
+
   return (
     <div>
-        <PricingPlan />
+      <PricingPlan />
     </div>
-  )
+  );
 }
 
-export default SubscriptionPage
+export default SubscriptionPage;
