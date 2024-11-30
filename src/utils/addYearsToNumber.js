22a5f766b3d41d6ac59c618f362f964f1e7fc100
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2024-11-30 21:20:46 +0100

    Update addYearsToNumber.js

--- src/utils/addYearsToNumber.js
+++ src/utils/addYearsToNumber.js
@@ -2,7 +2,7 @@ function addYearsToValues(integers) {
     if (integers === "" || integers === null || integers === undefined) {
       return "_______"; // Render the actual name if it exists
     } else {
-      const plural = integers !== 1 ? "s" : ""; // Add 's' for values other than 1
+      const plural = integers !== 1 ? "s" : "";
       return `${integers} month${plural}`;
     }
   }
