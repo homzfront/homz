7927c4e901321c06e4d578987917371f58a8b77e
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2024-12-04 11:40:47 +0100

    Update SubscriptionInfo.js

--- src/app/dashboard/list_Property/Profile/components/SubscriptionInfo.js
+++ src/app/dashboard/list_Property/Profile/components/SubscriptionInfo.js
@@ -1,27 +1,25 @@
 import React, {
-  useEffect,
   useState,
   useLayoutEffect,
-  useTransition,
 } from "react";
 import ConfirmationModal from "@/components/mainmenu/ConfirmationModal";
 import SuccessModal from "@/components/mainmenu/SuccessModal";
 import PromotionHooks from "@/utils/promoteProperty";
-import ThreeDotsLoader from "@/components/mainmenu/ThreeDotsLoader";
-import { useRouter } from "next/navigation";
+// import ThreeDotsLoader from "@/components/mainmenu/ThreeDotsLoader";
+// import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 
 const SubscriptionInfo = () => {
   const [isLoading, setLoader] = useState(false);
-  const [isLoading2, setLoader2] = useState(false);
+  // const [isLoading2, setLoader2] = useState(false);
   const [cancelPlan, setCancelPlan] = useState(false);
   const [planCancelledModal, setPlanCancelledModal] = useState(false);
   // const [restartPlanModal, setRestartPlanModal] = useState(false);
   const [currentPlanData, setCurrentPlanData] = useState();
-  const [isPending, startTransition] = useTransition();
+  // const [isPending, startTransition] = useTransition();
 
-  const router = useRouter();
+  // const router = useRouter();
 
   const getCurrentSubscriptionPlan = async () => {
     const response = await PromotionHooks.checkCurrentSubscription();
@@ -33,12 +31,12 @@ const SubscriptionInfo = () => {
     getCurrentSubscriptionPlan();
   }, []);
 
-  useEffect(() => {
-    if (isPending) {
-      return setLoader2(true);
-    }
-    setLoader2(false);
-  }, [isPending]);
+  // useEffect(() => {
+  //   if (isPending) {
+  //     return setLoader2(true);
+  //   }
+  //   setLoader2(false);
+  // }, [isPending]);
 
   const formatDateFunction = (dateString) => {
     const date = new Date(dateString);
@@ -69,18 +67,21 @@ const SubscriptionInfo = () => {
     // setRestartPlanModal(false);
   };
   return (
-    <div className="space-y-2 leading-[21px] font-[500] text-[14px]  pt-5 md:pt-0">
-      <p className="text-[#202020]  ">Subscription Plan</p>
+    <div className="space-y-2 leading-[21px] font-[500] text-[14px] pt-5 md:pt-0">
+      <p className="text-[#202020]">Subscription Plan</p>
       <div className="flex items-center justify-between flex-wrap h-fit py-[16px] px-[20px] sm:gap-[32px] gap-[20px] bg-[#F6F6F6] rounded-[8px]">
         <div className="space-y-1">
-            {/* currentPlanData?.plan?.name && !currentPlanData?.IsExpired && currentPlanData?.status =="active" */}
           <p className="">{`You’re currently on the ${
-            currentPlanData?.plan?.name
+            currentPlanData?.plan?.name &&
+            !currentPlanData?.IsExpired &&
+            currentPlanData?.status === "active"
               ? currentPlanData?.plan?.name
               : "Free Plan"
           }`}</p>
-          {/* {currentPlanData?.subscription_code && !currentPlanData?.IsExpired && currentPlanData?.status =="active" ( */}
-          {currentPlanData?.subscription_code(
+  
+          {currentPlanData?.subscription_code &&
+          !currentPlanData?.IsExpired &&
+          currentPlanData?.status === "active" && (
             <p className="flex gap-2 flex-wrap text-[#4E4E4E]">
               <span className="">{`${currentPlanData?.plan?.interval} subscription`}</span>
               <span className="sm:inline-block hidden">|</span>
@@ -99,21 +100,23 @@ const SubscriptionInfo = () => {
                 ? "/subscriptionPlans?upgrade=true"
                 : "/subscriptionPlans"
             }
-            className="bg-[#006AFF] text-white py-[8px] px-[12px] h-[37px] rounded-[4px] flex items-center w-full sm:w-[115px]  justify-center"
+            className="bg-[#006AFF] text-white py-[8px] px-[12px] h-[37px] rounded-[4px] flex items-center w-full sm:w-[115px] justify-center"
           >
             Upgrade Plan
           </Link>
-          {/* {!currentPlanData?.IsExpired && currentPlanData?.status =="active" ( */}
-          {currentPlanData?.subscription_code  (
-            <button
-              className="text-[#006AFF] bg-white py-[8px] px-[12px] h-[37px] rounded-[4px] flex items-center w-full sm:w-fit  justify-center border-[1px] border-[#006AFF]"
-              onClick={() => setCancelPlan(true)}
-            >
-              Cancel subscription
-            </button>
-          )}
+  
+          {!currentPlanData?.IsExpired &&
+            currentPlanData?.status === "active" && (
+              <button
+                className="text-[#006AFF] bg-white py-[8px] px-[12px] h-[37px] rounded-[4px] flex items-center w-full sm:w-fit justify-center border-[1px] border-[#006AFF]"
+                onClick={() => setCancelPlan(true)}
+              >
+                Cancel subscription
+              </button>
+            )}
         </div>
       </div>
+  
       <ConfirmationModal
         isOpen={cancelPlan}
         title="Cancel Subscription?"
@@ -126,7 +129,6 @@ const SubscriptionInfo = () => {
         optionText="Proceed"
         optionText2="Cancel"
         isLoading={isLoading}
-        // color="text-[#D92D20]"
       />
       <SuccessModal
         isOpen={planCancelledModal}
@@ -135,9 +137,7 @@ const SubscriptionInfo = () => {
         buttonColor={true}
         handleEvent={closeSuccessModal}
       />
-    
     </div>
   );
-};
-
+}  
 export default SubscriptionInfo;
