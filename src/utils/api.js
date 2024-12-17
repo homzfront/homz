56dd18192624b4e8db27fdbd46beba8b4505e94c
lordyemight <lordyemight@gmail.com>
2024-12-17 14:25:11 +0100

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import "dotenv/config";
+import "dotenv/config"
+// import Cookies from 'js-cookie';
 
 // Create an instance of Axios with custom configuration
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
-  withCredentials: false, // Set to true if cookies are needed for cross-origin requests
+  withCredentials: false,
+  // credentials: 'include'
 });
 
 // Add an interceptor to include the JWT token in headers for every request
