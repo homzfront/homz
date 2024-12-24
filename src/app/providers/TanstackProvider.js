0701ec8b3d5026caffa5407b4982286df0d68489
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2024-12-24 02:42:33 +0100

    Update TanstackProvider.js

--- src/app/providers/TanstackProvider.js
+++ src/app/providers/TanstackProvider.js
@@ -3,12 +3,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const TanstackProvider = ({ children }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1, // Retry failed queries once
+        refetchOnWindowFocus: false, // Avoid refetching on window focus
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />.
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
