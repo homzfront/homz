7e18708d9b4ab3b37ad47456de67c6009995d0a5
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2025-01-02 20:58:56 +0100

    update

--- src/app/dashboard/tenant/dashboard/layout.js
+++ src/app/dashboard/tenant/dashboard/layout.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Header from "@/pages/dashboard/tenant/header/header";
 import Sidebar from "@/pages/dashboard/tenant/sidebar/sidebar";
-import { TanstackProvider } from "@/app/providers/TanstackProvider";
 const Layout = ({ children }) => {
   return (
     <div className="dashboard_main">
       <Sidebar />
       <div className="w-full">
         <Header />
-        <TanstackProvider>
-          <div>{children}</div>
-        </TanstackProvider>
-        {/* {children} */}
+        {children}
       </div>
     </div>
   );
