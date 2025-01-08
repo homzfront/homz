34a3faadbb81a94590cbccd137944369dbeaaec9
lordyemight <lordyemight@gmail.com>
2025-01-08 09:10:57 +0100

    SingleTenant & Tenant Account Creation

--- src/app/api/redirect/redirect.js
+++ src/app/api/redirect/redirect.js
@@ -9,7 +9,7 @@ export const config = {
         const res = await fetch(url.searchParams.get('url')); // Fetch the external URL
         const contentType = res.headers.get('content-type');
         return NextResponse.rewrite(url.searchParams.get('url'))
-          .header('Content-Type', contentType); // Set the correct content type
+          .header('Content-Type', contentType); // Set the correct content type.
       } catch (error) {
         console.error('Error fetching external URL:', error);
         // Handle errors as needed, e.g., redirect to an error page
