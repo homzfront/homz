dbb632c73dfd2cbf0d0212f89526bf70a4d098af
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2025-02-10 10:28:39 +0100

    Update widget.js

--- src/app/subscriptionPlans/widget.js
+++ src/app/subscriptionPlans/widget.js
@@ -8,12 +8,12 @@ import SuccessModal from "@/components/mainmenu/SuccessModal";
 import useStorePropertyPromotionData from "@/store/propertyPromotions.js";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useRouter } from "next/navigation";
-import ConfirmPaymentModal from "./components/confirmPaymentModal.js";
+// import ConfirmPaymentModal from "./components/confirmPaymentModal.js";
 
 const Widget = ({ data, profile }) => {
   const [successModalIsOpen, setSuccessModalIsOpen] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [isConfirmPayOpen, setIsConfirmOpenModal] = useState(false);
+
   const [upgradePlan, setUpgradePlan] = useState("");
 
   const router = useRouter();
@@ -29,23 +29,24 @@ const Widget = ({ data, profile }) => {
     setUpgradePlan(upgrade);
   }, [upgradePlan]);
 
-  useEffect(() => {
-    if (!profile?.data?.paid_at || !profile?.data?.status) return; // Ensure both exist before proceeding
 
-    const paidAt = new Date(profile.data.paid_at);
-    const today = new Date();
+  // useEffect(() => {
+  //   if (!profile?.data?.paid_at || !profile?.data?.status) return; // Ensure both exist before proceeding
 
-    const formatDate = (date) => date.toISOString().split("T")[0];
+  //   const paidAt = new Date(profile.data.paid_at);
+  //   const today = new Date();
 
-    const isSameDate = formatDate(paidAt) === formatDate(today);
-    const isValidStatus = ["success", "non-renewing"].includes(
-      profile.data.status
-    );
+  //   const formatDate = (date) => date.toISOString().split("T")[0];
 
-    if (isSameDate && isValidStatus) {
-      setIsConfirmOpenModal(true);
-    }
-  }, [profile?.data?.paid_at, profile?.data?.status]);
+  //   const isSameDate = formatDate(paidAt) === formatDate(today);
+  //   const isValidStatus = ["success", "non-renewing"].includes(
+  //     profile.data.status
+  //   );
+
+  //   if (isSameDate && isValidStatus) {
+  //     setIsConfirmOpenModal(true);
+  //   }
+  // }, [profile?.data?.paid_at, profile?.data?.status]);
 
   const pages = [
     {
@@ -137,48 +138,47 @@ const Widget = ({ data, profile }) => {
         successText={`Your ${"[Monthly]"} promotion is currently running for this property`}
       />
 
-      <ConfirmPaymentModal
-        isOpen={isConfirmPayOpen}
-        setIsOpenModal={setIsConfirmOpenModal}
-      />
+    
     </div>
   );
 };
 
 export default Widget;
-const customTheme = {
-  root: {
-    base: "relative h-full w-full",
-    leftControl:
-      "hidden md:inline-block absolute top-[rem] left-[-0.25rem] flex h-[30px] w-[30px] items-center justify-center px-4 focus:outline-none transition-opacity duration-300",
-    rightControl:
-      "hidden md:inline-block absolute top-[rem] right-7 flex h-[30px] w-[30px] items-center justify-center px-4 focus:outline-none transition-opacity duration-300",
-    "&:hover $leftControl, &:hover $rightControl": {
-      display: "inline-block",
-    },
-  },
-  indicators: {
-    active: {
-      off: " bg-[#559CFF] hover:bg-white dark:bg-blue-600/50 dark:hover:bg-gray-800 ",
-      on: " bg-[#EEF5FF] dark:bg-gray-800",
-    },
-    base: "h-[8px] w-[8px] rounded-full ",
-    wrapper:
-      "absolute bottom-[-25px] w-full  items-center flex justify-center  sm:left-1/2 left-[120px] -translate-x-1/2  space-x-1 ",
-  },
-  item: {
-    base: "absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2",
-    wrapper: {
-      off: "w-full flex-shrink-0 transform cursor-default snap-center",
-      on: "w-full flex-shrink-0 transform cursor-grab snap-center",
-    },
-  },
-  control: {
-    base: "hidden md:inline-flex h-[20px] w-[20px] px-2 items-center bg-[#292D32] justify-center border rounded-[8px] bg-opacity-10 hover:bg-opacity-50  group-focus:outline-none group-focus:ring-1 border-[#559CFF] group-focus:ring-[#EEF5FF] dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60  dark:group-focus:ring-gray-800/70 sm:h-[30px] sm:w-[30px]",
-    icon: "h-3 w-3 text-white/60 dark:text-gray-800 group-hover:text-white sm:h-6 sm:w-6",
-  },
-  scrollContainer: {
-    base: "flex h-full snap-mandatory overflow-y-hidden overflow-x-hidden scroll-smooth rounded-lg",
-    snap: "snap-x",
-  },
-};
+
+
+// const customTheme = {
+//   root: {
+//     base: "relative h-full w-full",
+//     leftControl:
+//       "hidden md:inline-block absolute top-[rem] left-[-0.25rem] flex h-[30px] w-[30px] items-center justify-center px-4 focus:outline-none transition-opacity duration-300",
+//     rightControl:
+//       "hidden md:inline-block absolute top-[rem] right-7 flex h-[30px] w-[30px] items-center justify-center px-4 focus:outline-none transition-opacity duration-300",
+//     "&:hover $leftControl, &:hover $rightControl": {
+//       display: "inline-block",
+//     },
+//   },
+//   indicators: {
+//     active: {
+//       off: " bg-[#559CFF] hover:bg-white dark:bg-blue-600/50 dark:hover:bg-gray-800 ",
+//       on: " bg-[#EEF5FF] dark:bg-gray-800",
+//     },
+//     base: "h-[8px] w-[8px] rounded-full ",
+//     wrapper:
+//       "absolute bottom-[-25px] w-full  items-center flex justify-center  sm:left-1/2 left-[120px] -translate-x-1/2  space-x-1 ",
+//   },
+//   item: {
+//     base: "absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2",
+//     wrapper: {
+//       off: "w-full flex-shrink-0 transform cursor-default snap-center",
+//       on: "w-full flex-shrink-0 transform cursor-grab snap-center",
+//     },
+//   },
+//   control: {
+//     base: "hidden md:inline-flex h-[20px] w-[20px] px-2 items-center bg-[#292D32] justify-center border rounded-[8px] bg-opacity-10 hover:bg-opacity-50  group-focus:outline-none group-focus:ring-1 border-[#559CFF] group-focus:ring-[#EEF5FF] dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60  dark:group-focus:ring-gray-800/70 sm:h-[30px] sm:w-[30px]",
+//     icon: "h-3 w-3 text-white/60 dark:text-gray-800 group-hover:text-white sm:h-6 sm:w-6",
+//   },
+//   scrollContainer: {
+//     base: "flex h-full snap-mandatory overflow-y-hidden overflow-x-hidden scroll-smooth rounded-lg",
+//     snap: "snap-x",
+//   },
+// };
