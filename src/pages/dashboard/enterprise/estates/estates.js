9b0b8ddebb2012ddbac24eaea87cee19a394ac18
lordyemight <lordyemight@gmail.com>
2025-02-24 16:58:37 +0100

    first commit

--- src/pages/dashboard/enterprise/estates/estates.js
+++ src/pages/dashboard/enterprise/estates/estates.js
@@ -124,7 +124,9 @@ const Estate = () => {
       setOpenPurchasePlan(!openPurchasePlan);
     } else if (reachedLimit?.reachedMaxTenants) {
       setOpenPurchasePlan(!openPurchasePlan);
-    } else if (isTrialExpired(user?.trialEndDate)) {
+    } else if (isTrialExpired(user?.trialEndDate) && ((user?.planName === "Enterprise Free") || (user?.planName === "Enterprise Trial"))) {
+      setOpenPurchasePlan(!openPurchasePlan);
+    } else if (reachedLimit?.expiredPlan) {
       setOpenPurchasePlan(!openPurchasePlan);
     } else {
       setRegistrationForm(true);
