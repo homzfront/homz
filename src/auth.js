a82f0dcbb408242d29bd53ae24c460dda1a64c1e
lordyemight <lordyemight@gmail.com>
2025-05-02 13:34:33 +0100

    changed env

--- src/auth.js
+++ src/auth.js
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
       authorization: {
         params: {
           prompt: "consent",
@@ -28,3 +28,4 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
+
