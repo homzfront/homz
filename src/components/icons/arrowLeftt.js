987278c4033debf6bd3346c5bba961497b1c5039
lordyemight <lordyemight@gmail.com>
2025-06-27 09:39:42 +0100

    referral code

---
+++ src/components/icons/arrowLeftt.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ArrowLeftt = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.57 5.92969L3.5 11.9997L9.57 18.0697" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M20.5019 12H3.67188" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default ArrowLeftt
---
+++ src/components/icons/referral.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Referral = () => {
+    return (
+        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.5651 6.91406H2.89844V12.2474C2.89844 14.2474 3.5651 14.9141 5.5651 14.9141H10.8984C12.8984 14.9141 13.5651 14.2474 13.5651 12.2474V6.91406Z" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M14.5807 4.91927V5.58594C14.5807 6.31927 14.2274 6.91927 13.2474 6.91927H3.2474C2.2274 6.91927 1.91406 6.31927 1.91406 5.58594V4.91927C1.91406 4.18594 2.2274 3.58594 3.2474 3.58594H13.2474C14.2274 3.58594 14.5807 4.18594 14.5807 4.91927Z" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M8.00921 3.58427H4.32921C4.10254 3.3376 4.10921 2.9576 4.34921 2.7176L5.29587 1.77094C5.54254 1.52427 5.94921 1.52427 6.19587 1.77094L8.00921 3.58427Z" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M12.1644 3.58427H8.48438L10.2977 1.77094C10.5444 1.52427 10.951 1.52427 11.1977 1.77094L12.1444 2.7176C12.3844 2.9576 12.391 3.3376 12.1644 3.58427Z" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M6.21094 6.91406V10.3407C6.21094 10.8741 6.7976 11.1874 7.24427 10.9007L7.87094 10.4874C8.0976 10.3407 8.38427 10.3407 8.60427 10.4874L9.1976 10.8874C9.6376 11.1807 10.2309 10.8674 10.2309 10.3341V6.91406H6.21094Z" stroke="#4E4E4E" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+export default Referral
