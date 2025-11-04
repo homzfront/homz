9b5d21f59b4a87c88a72b4f1c1218f72443db6a2
lordyemight <lordyemight@gmail.com>
2025-11-04 10:00:04 +0100

    Added new meta tage in (properties layout)

--- src/app/(properties)/layout.js
+++ src/app/(properties)/layout.js
@@ -2,16 +2,16 @@ import Footer from "/src/components/layout/Footer";
 import Header from "/src/components/layout/Header";
 
 export const metadata = {
-  title: 'Explore Properties Across Nigeria',
-  description: 'Browse verified homes, land, and shortlets for sale or rent in top Nigerian locations.', // ← Your custom tag
+  title: 'Property Management Software Nigeria - Rent, Facility & Estate',
+  description: 'Your property management software for landlords, agents, facility/estate managers for document generation, finance, tracking and access control.', // ← Your custom tag
   openGraph: {
-    title: 'Explore Properties Across Nigeria',
-    description: 'Browse verified homes, land, and shortlets for sale or rent in top Nigerian locations.',
+    title: 'Property Management Software Nigeria - Rent, Facility & Estate',
+    description: 'Your property management software for landlords, agents, facility/estate managers for document generation, finance, tracking and access control.',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Explore Properties Across Nigeria',
-    description: 'Browse verified homes, land, and shortlets for sale or rent in top Nigerian locations.',
+    title: 'Property Management Software Nigeria - Rent, Facility & Estate',
+    description: 'Your property management software for landlords, agents, facility/estate managers for document generation, finance, tracking and access control.',
   }
 }
 
