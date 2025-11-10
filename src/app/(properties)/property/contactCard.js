6d87beff9afa000f1d908c1386fd938f05a7a3ae
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2025-11-10 12:59:12 +0100

    Update contactCard.js

--- src/app/(properties)/property/contactCard.js
+++ src/app/(properties)/property/contactCard.js
@@ -24,16 +24,44 @@ const ContactCard = ({
               ? contactData?.contacts?.phoneNumber
               : formatNumber(contactData?.contacts?.phoneNumber)}
           </p>
+
           <button
-            className="text-white bg-[#006AFF] py-[4px] px-[12px] rounded-[8px]  text-[11px] leading-[16.5px] font-[400]"
+            className="text-white bg-[#006AFF] py-[4px] px-[12px] rounded-[8px] text-[11px] leading-[16.5px] font-[400]"
             onClick={() => {
               setShowNumber(!showNumber);
-              if (!showNumber) updateMetrics("call");
+
+              if (!showNumber) {
+                updateMetrics("call");
+
+                let phone = contactData?.contacts?.phoneNumber?.replace(
+                  /\s+/g,
+                  ""
+                );
+                if (phone && !phone.startsWith("+234")) {
+                  if (phone.startsWith("0")) {
+                    phone = `+234${phone.slice(1)}`;
+                  } else {
+                    phone = `+234${phone}`;
+                  }
+                }
+
+                const isMobile = /iPhone|iPad|iPod|Android/i.test(
+                  navigator.userAgent
+                );
+
+                if (isMobile) {
+                  window.location.href = `tel:${phone}`;
+                } else {
+                  const whatsappURL = `https://wa.me/${phone.replace("+", "")}`;
+                  window.open(whatsappURL, "_blank");
+                }
+              }
             }}
           >
             Call Agent
           </button>
         </div>
+
         {contactData?.contacts?.whatsapp && (
           <div className="bg-white sm:w-[280px] h-[44px] flex items-center justify-between p-[12px] rounded-[8px]">
             <p className="text-[#039855] text-[13px] flex gap-2 font-[400] leading-[19.5px]">
